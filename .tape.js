9e9fcdc1d0dbc9cd2b859432c7844fa77cdf7b66
Jonathan Neal <jonathantneal@hotmail.com>
2019-01-23 10:54:15 -0500

    4.0.0

--- .tape.js
+++ .tape.js
@@ -6,47 +6,47 @@ const processOptions = {
 };
 
 module.exports = {
-	'postcss-sass': {
-		'basic': {
-			message: 'supports basic usage',
-			processOptions: processOptions,
-			source: 'basic.scss'
+	'basic': {
+		message: 'supports basic usage',
+		processOptions,
+		source: 'basic.scss'
+	},
+	'basic:sassopts': {
+		message: 'supports sass options usage',
+		options: {
+			indentType: 'tab',
+			indentWidth: 1,
+			outputStyle: 'expanded'
 		},
-		'basic:sassopts': {
-			message: 'supports sass options usage',
-			options: {
-				indentType: 'tab',
-				indentWidth: 1,
-				outputStyle: 'expanded'
-			},
-			processOptions: processOptions,
-			source: 'basic.scss'
-		},
-		'basic:mixed': {
-			message: 'supports mixed (postcss-unroot, postcss-sass) usage',
-			plugin: () => require('postcss')(
-				require('postcss-unroot'),
-				require('.')
-			),
-			processOptions: processOptions,
-			source: 'basic.scss'
-		},
-		'imports': {
-			message: 'supports imports usage',
-			plugin: () => require('postcss')(
-				require('.')
-			),
-			processOptions: processOptions,
-			source: 'imports.scss'
-		},
-		'css-imports': {
-			message: 'supports imports (postcss-import, postcss-sass) usage',
-			plugin: () => require('postcss')(
-				require('postcss-import'),
-				require('.')
-			),
-			processOptions: processOptions,
-			source: 'css-imports.scss'
-		}
+		processOptions,
+		source: 'basic.scss'
+	},
+	'basic:mixed': {
+		message: 'supports mixed (postcss-unroot, postcss-sass) usage',
+		plugin: require('postcss')([
+			require('postcss-unroot'),
+			require('.')
+		]),
+		processOptions,
+		source: 'basic.scss'
+	},
+	'imports': {
+		message: 'supports imports usage',
+		plugin: require('postcss')(
+			require('.')
+		),
+		processOptions,
+		source: 'imports.scss'
+	},
+	'postcss-imports': {
+		message: 'supports imports (postcss-import, postcss-sass) usage',
+		plugin: require('postcss')(
+			require('postcss-import'),
+			require('.')
+		),
+		processOptions: Object.assign({}, processOptions, {
+			syntax: require('postcss-scss')
+		}),
+		source: 'postcss-imports.scss'
 	}
 };
