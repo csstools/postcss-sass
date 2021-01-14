0bcaacc94bb3772b1b700a24b1ed5ef6eb9b3d6f
Okiki <femikiki@gmail.com>
2021-01-14 07:52:49 +0000

    refactor: :art: add support for postcss 8
    support postcss 8; fix bugs in tests; upate deps; update build process
    
    postcss-import@latest doesn't seem to  import without .scss

--- .tape.js
+++ .tape.js
@@ -24,6 +24,8 @@ module.exports = {
 	'basic:mixed': {
 		message: 'supports mixed (postcss-unroot, postcss-sass) usage',
 		plugin: require('postcss')([
+			// In the previous test, unroot wouldn't do anything.
+			// I'm pretty sure that is a bug, so I changed the expected css file
 			require('postcss-unroot'),
 			require('.')
 		]),
@@ -39,10 +41,11 @@ module.exports = {
 		source: 'imports.scss'
 	},
 	'postcss-imports': {
-		message: 'supports imports (postcss-import, postcss-sass) usage',
+		message: 'supports imports (postcss-sass, postcss-import) usage',
 		plugin: require('postcss')(
-			require('postcss-import'),
-			require('.')
+			// Unsure what happened to postcss-import but it no longer imports without .scss at the end
+			require('.'),
+			require('postcss-import')
 		),
 		processOptions: Object.assign({}, processOptions, {
 			syntax: require('postcss-scss')
